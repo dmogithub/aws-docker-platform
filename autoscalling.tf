39c9129a4fe9cb6dc3c207ae9c3dda44a72e21b2
dmogithub <dmo95@orange.fr>
2017-10-12 15:30:03 +0200

    Update autoscalling.tf

--- autoscalling.tf
+++ autoscalling.tf
@@ -1,49 +1,51 @@
-resource "aws_placement_group" "test" {
-  name     = "test"
-  strategy = "cluster"
+resource "aws_launch_configuration" "docker-nodes" {
+    name_prefix = "docker-nodes-"
+    image_id = "ami-bb9a6bc2"
+    instance_type = "t2.micro"
+    user_data = "sudo yum -y update "
+
+    lifecycle {
+        create_before_destroy = true
+    }
+
+    root_block_device {
+        volume_type = "gp2"
+        volume_size = "50"
+    }
 }
+resource "aws_autoscaling_group" "workers" {
+    availability_zones = ["eu-west-1a"]
+    vpc_zone_identifier = ["subnet-062a5361"]
+    name = "workers"
+    max_size = "5"
+    min_size = "1"
+    health_check_grace_period = 300
+    health_check_type = "EC2"
+    desired_capacity = 2
+    force_delete = true
+    launch_configuration = "${aws_launch_configuration.docker-nodes.name}"
 
-resource "aws_autoscaling_group" "bar" {
-  availability_zones        = ["us-east-1a"]
-  name                      = "foobar3-terraform-test"
-  max_size                  = 5
-  min_size                  = 2
-  health_check_grace_period = 300
-  health_check_type         = "ELB"
-  desired_capacity          = 4
-  force_delete              = true
-  placement_group           = "${aws_placement_group.test.id}"
-  launch_configuration      = "${aws_launch_configuration.foobar.name}"
-
-  initial_lifecycle_hook {
-    name                 = "foobar"
-    default_result       = "CONTINUE"
-    heartbeat_timeout    = 2000
-    lifecycle_transition = "autoscaling:EC2_INSTANCE_LAUNCHING"
-
-    notification_metadata = <<EOF
-{
-  "foo": "bar"
+    tag {
+        key = "Name"
+        value = "worker Instance"
+        propagate_at_launch = true
+    }
 }
-EOF
+resource "aws_autoscaling_group" "managers" {
+    availability_zones = ["eu-west-1c"]
+    vpc_zone_identifier = ["subnet-2151eb7a"]
+    name = "managers"
+    max_size = "5"
+    min_size = "1"
+    health_check_grace_period = 300
+    health_check_type = "EC2"
+    desired_capacity = 1
+    force_delete = true
+    launch_configuration = "${aws_launch_configuration.docker-nodes.name}"
 
-    notification_target_arn = "arn:aws:sqs:us-east-1:444455556666:queue1*"
-    role_arn                = "arn:aws:iam::123456789012:role/S3Access"
-  }
-
-  tag {
-    key                 = "foo"
-    value               = "bar"
-    propagate_at_launch = true
-  }
-
-timeouts {
-    delete = "15m"
-}
-
-  tag {
-    key                 = "lorem"
-    value               = "ipsum"
-    propagate_at_launch = false
-  }
+    tag {
+        key = "Name"
+        value = "worker Instance"
+        propagate_at_launch = true
+    }
 }
